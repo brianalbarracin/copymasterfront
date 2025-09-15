badf930497b94f7366c55e0b59593a6d93d0be7e
Brian Albarracín <baalbarracinb@unal.edu.co>
2025-09-15 12:34:46 -0500

    Actualizaciones services 5

--- assets/js/technicianService.js
+++
@@ -1,8 +0,0 @@
-const technicianService = {
-  list: ()=> fetch(API_BASE + "/technicians").then(r=>r.json()),
-  create: (data)=> fetch(API_BASE + "/technicians", {
-    method: "POST", 
-    headers: {"Content-Type": "application/json"}, 
-    body: JSON.stringify(data)
-  }).then(r=>r.json())
-};
