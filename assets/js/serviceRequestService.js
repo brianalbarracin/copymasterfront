bf1f5ec006122d24316301b2e728355a7800276f
Brian Albarracín <baalbarracinb@unal.edu.co>
2025-09-15 12:15:13 -0500

    Actualizaciones services 3

--- assets/js/serviceRequestService.js
+++
@@ -1,5 +0,0 @@
-const serviceRequestService = {
-  list: ()=> fetch(API_BASE + "/api/service-requests").then(r=>r.json()),
-  get: (id)=> fetch(API_BASE + "/api/service-requests/"+id).then(r=>r.json()),
-  create: (data)=> fetch(API_BASE + "/api/service-requests", {method:"POST", headers:{"Content-Type":"application/json"}, body: JSON.stringify(data)}).then(r=>r.json())
-};
